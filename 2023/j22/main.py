d8f3ac4d9389cf515709e541126b0040d1e04bd9
Gabin <110061374+Gabin221@users.noreply.github.com>
2024-03-15 20:12:21 +0100

    Update main.py

---
+++ 2023/j22/main.py
@@ -0,0 +1,24 @@
+import re
+from operator import mul
+from queue import Queue
+import numpy as np 
+from collections import Counter, defaultdict
+from math import prod
+import sys
+from typing import List
+from time import perf_counter
+from matplotlib.path import Path
+from time import perf_counter
+from itertools import combinations
+from tqdm import tqdm
+from pprint import pprint
+from copy import deepcopy
+import heapq
+from utils import *
+from cProfile import run
+from functools import cache
+from sympy import var, Eq, solve
+import networkx
+import random
+
+sys.setrecursionlimit(999999)
