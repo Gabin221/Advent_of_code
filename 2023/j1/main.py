184b407d68c894e3edbcbca7cd65a80eb894a60f
Gabin221 <serrurot19@gmail.com>
2024-05-27 15:18:50 +0200

    Update main.py

--- 2023/j1/main.py
+++ 2023/j1/main.py
@@ -3,36 +3,15 @@ import sys
 
 # sys.setrecursionlimit(999999)
 
-def premierNbr(ligne):
-    liste = ["1", "one", "2", "two", "3", "three", "4", "four", "5", "five", "6", "six", "7", "seven", "8", "eight", "9", "nine"]
-    premier = ""
+def transformChaine(ligne):
+    listeEntier = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    liste = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    newLine = ligne
 
     for i in range(0, len(liste)):
-        if liste[i] in ligne:
-            if i%2 == 0:
-                premier += liste[i]
-            else:
-                premier += liste[i - 1]
-            break
-    return premier
+        newLine.replace(liste[i], listeEntier[i])
 
-
-def dernierNbr(ligne):
-    liste = ["1", "one", "2", "two", "3", "three", "4", "four", "5", "five", "6", "six", "7", "seven", "8", "eight", "9", "nine"]
-    positions = []
-
-    for i in liste:
-        if i in ligne:
-            positions.append(liste.index(i))
-        else:
-            positions.append(19)
-
-    dernier = int([i for i in positions if i != 19][-1])
-    if dernier%2 == 0:
-        return liste[dernier]
-    else:
-        return liste[dernier-1]
-
+    return newLine
 
 def j1():
     filename = "../Input/inputj1.txt"
@@ -54,8 +33,12 @@ def j1():
     def partie2():
         total = 0
         for line in input:
-            total += int(premierNbr(line) + dernierNbr(line))
-            print(f"{line}: {int(premierNbr(line) + dernierNbr(line))}")
+            chaine = ""
+            for i in transformChaine(line):
+                if i.isdigit():
+                    chaine += i
+
+            total += int(chaine[0] + chaine[-1])
         
         print(f"Réponse jour 1 partie 2 : {total}")
     
