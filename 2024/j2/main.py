e2703d9bc55043740f358c9e144d23b5a33f1bb1
Gabin221 <serrurot19@gmail.com>
2024-12-02 20:30:14 +0100

    Update main.py

--- 2024/j2/main.py
+++ 2024/j2/main.py
@@ -1,22 +1,22 @@
 with open("../Input/j2.txt","r",encoding="utf8") as file :
-    lignes = [line.strip() for line in file.readlines()]
+	lignes = [line.strip() for line in file.readlines()]
 
 
 def croissant(liste):
-    for i in range(len(liste)-1):
-        if not liste[i] <= liste[i+1]:
-            return False
-    return True
+	for i in range(len(liste)-1):
+		if not liste[i] <= liste[i+1]:
+			return False
+	return True
 
 
 def decroissant(liste):
-    for i in range(len(liste)-1):
-        if not liste[i] >= liste[i+1]:
-            return False
-    return True
+	for i in range(len(liste)-1):
+		if not liste[i] >= liste[i+1]:
+			return False
+	return True
 
 
-def rapport_sur(liste):
+def rapport_sur_partie1(liste):
 	sur = False
 	for i in range(1, len(liste)):
 		if abs(liste[i] - liste[i - 1]) >= 1 and abs(liste[i] - liste[i - 1]) <= 3 and (decroissant(liste) or croissant(liste)):
@@ -26,6 +26,28 @@ def rapport_sur(liste):
 			break
 	
 	return sur
+
+
+def rapport_sur_partie2(liste):
+	sur = False
+	for i in range(1, len(liste)):
+		if abs(liste[i] - liste[i - 1]) < 1 or abs(liste[i] - liste[i - 1]) > 3:
+			break
+	else:
+		if croissant(liste) or decroissant(liste):
+			return True
+	
+	for i in range(len(liste)):
+		new_liste = liste[:i] + liste[i+1:]
+		sur = True
+		for j in range(1, len(new_liste)):
+			if abs(new_liste[j] - new_liste[j - 1]) < 1 or abs(new_liste[j] - new_liste[j - 1]) > 3:
+				sur = False
+				break
+		if sur and (croissant(new_liste) or decroissant(new_liste)):
+			return True
+
+	return False
 
 
 def partie1():
@@ -40,16 +62,28 @@ def partie1():
 		rapports.append(rapport)
 
 	for liste in rapports:
-		if rapport_sur(liste):
+		if rapport_sur_partie1(liste):
 			rapports_surs += 1
 
 	print(f"La réponse de la partie 1 est {rapports_surs}")
 
 
 def partie2():
-	score = 0
+	rapports_surs = 0
+	rapports = []
+	
+	for ligne in lignes:
+		rapport = ligne.split(" ")
+		for i in range(len(rapport)):
+			rapport[i] = int(rapport[i])
 
-	print(f"La réponse de la partie 2 est {score}")
+		rapports.append(rapport)
+
+	for liste in rapports:
+		if rapport_sur_partie2(liste):
+			rapports_surs += 1
+
+	print(f"La réponse de la partie 2 est {rapports_surs}")
 
 
 if __name__ == "__main__":
