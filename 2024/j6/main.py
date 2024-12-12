2d2cd2944aa40313f5c51cda8c60ef387e6888a2
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-12-12 19:03:10 +0100

    Update main.py

--- 2024/j6/main.py
+++ 2024/j6/main.py
@@ -41,7 +41,6 @@ def partie2():
     tourne_droite = {"^": ">", ">": "v", "v": "<", "<": "^"}
     possible_positions = []
 
-    # Trouver la position et direction initiales du garde
     for ligne in range(len(matrice)):
         for colonne in range(len(matrice[ligne])):
             if matrice[ligne][colonne] in directions:
@@ -49,21 +48,20 @@ def partie2():
                 guard_direction = matrice[ligne][colonne]
                 break
 
-    # Tester toutes les positions accessibles (".") comme potentiel obstacle
     for ligne in range(len(matrice)):
         for colonne in range(len(matrice[ligne])):
             if matrice[ligne][colonne] != "." or (ligne, colonne) == guard_start:
                 continue
 
-            matrice[ligne][colonne] = "#"  # Ajouter une obstruction temporaire
+            matrice[ligne][colonne] = "#"
             position = guard_start
             direction = guard_direction
             visited = {}
             loop_detected = False
-            steps = 0  # Compteur pour éviter des boucles infinies
+            steps = 0
 
             while True:
-                # Utiliser un tuple (position, direction) pour détecter les boucles
+        
                 state = (position, direction)
                 if state in visited:
                     loop_detected = True
@@ -73,24 +71,23 @@ def partie2():
                 delta = directions[direction]
                 next_position = (position[0] + delta[0], position[1] + delta[1])
 
-                # Si le garde sort des limites
+        
                 if not (0 <= next_position[0] < len(matrice) and 0 <= next_position[1] < len(matrice[0])):
                     break
 
-                # Si le garde rencontre un obstacle, il tourne
                 if matrice[next_position[0]][next_position[1]] == "#":
                     direction = tourne_droite[direction]
                 else:
                     position = next_position
 
                 steps += 1
-                if steps > 10000:  # Limite de sécurité
+                if steps > 10000:
                     break
 
             if loop_detected:
                 possible_positions.append((ligne, colonne))
 
-            matrice[ligne][colonne] = "."  # Restaurer l'état initial
+            matrice[ligne][colonne] = "."
 
     print(f"La réponse de la partie 2 est {len(possible_positions)}")
 
