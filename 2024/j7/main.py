ab8b5ef71b2ee6b3dc1f8c1e24c81a47c2081809
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-12-12 19:23:41 +0100

    Update main.py

--- 2024/j7/main.py
+++ 2024/j7/main.py
@@ -1,9 +1,37 @@
+from itertools import product
+
 with open("../Input/j7.txt","r",encoding="utf8") as file :
-    lignes = [line.strip() for line in file.readlines()]
+	lignes = [line.strip() for line in file.readlines()]
+
+
+def evaluer_equation(nombres, operateurs):
+	resultat = nombres[0]
+	for i in range(len(operateurs)):
+		if operateurs[i] == '+':
+			resultat += nombres[i + 1]
+		elif operateurs[i] == '*':
+			resultat *= nombres[i + 1]
+	return resultat
 
 
 def partie1():
 	answer = 0
+
+	for ligne in lignes:
+		resultat_attendu = int(ligne.split(":")[0])
+		nombres = list(map(int, ligne.split(":")[1].strip().split()))
+		
+		nb_operateurs = len(nombres) - 1
+		combinaisons_operateurs = product("+*", repeat=nb_operateurs)
+		
+		equation_valide = False
+		for operateurs in combinaisons_operateurs:
+			if evaluer_equation(nombres, operateurs) == resultat_attendu:
+				equation_valide = True
+				break
+		
+		if equation_valide:
+			answer += resultat_attendu
 
 	print(f"La réponse de la partie 1 est {answer}")
 
